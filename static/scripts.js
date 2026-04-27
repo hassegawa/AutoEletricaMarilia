800150f477ddc70d98ccb01272e143caf868a8db
Hassegawa <hassegawa@outlook.com>
2026-04-26 21:58:16 -0300

    root

--- static/scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Lógica para abrir/fechar o menu lateral
-    const sidebar = document.querySelector('.sidebar');
-    const navbarToggler = document.querySelector('.navbar-toggler');
-
-    navbarToggler.addEventListener('click', function () {
-        sidebar.classList.toggle('active');
-    });
-});
